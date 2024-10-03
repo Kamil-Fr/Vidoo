552827a31344bf15a4072e8d20c3b80f8c6a029a
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-10-03 17:52:23 +0200

    New.

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Tabs, Redirect } from "expo-router";
+import { icons } from '../../constants';
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+  <View>
+    <Image source={} 
+    />
+  </View>
+  )
+  }
 
 const TabsLayout = () => {
   return (
     <>
       <Tabs>
-        <Tabs.Screen name="home" />
+        <Tabs.Screen name="home"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon 
+            icon={icons.home}
+            color={color}
+            />
+          )
+        }} />
       </Tabs>
     </>
   );
