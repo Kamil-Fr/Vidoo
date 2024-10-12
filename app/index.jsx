eea5cf391123d2330ed2240524ce31b73bf7c8c5
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-10-12 18:14:42 +0200

    Add new things to index.jsx.

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,31 @@ export default function App() {
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View className="w-full justify-center items-center h-full px-4">
-          <Image source={images.logo} />
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possiblities with{" "}
+              <Text className="text-secondary-200">Vidoo</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovations: embark on a journey of limitless
+            exploration with Vidoo
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
