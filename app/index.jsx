2b38ad78e3039778e05fad12a0d09a2b89971cfc
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-10-12 18:37:24 +0200

    Add CustomButton.

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,15 @@
 import { StatusBar, ScrollView, Image } from "react-native";
 import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -32,10 +33,16 @@ export default function App() {
           </View>
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
             Where creativity meets innovations: embark on a journey of limitless
-            exploration with Vidoo
+            exploration with Vidoo!
           </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
 }
