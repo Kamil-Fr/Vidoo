4ecb9d150bfb4397680f9b3816b8c37436d8abaf
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-10-13 12:52:46 +0200

    Create _layout.jsx in auth.

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
+import { ScreenStack } from "react-native-screens";
+import { StatusBar } from "expo-status-bar";
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <ScreenStack name="sign-in" options={{ headerShown: false }} />
+        <ScreenStack name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   );
 };
 
 export default AuthLayout;
-
-const styles = StyleSheet.create({});
--- app/index.jsx
+++ app/index.jsx
@@ -12,7 +12,7 @@ export default function App() {
         <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
-            className="w-[130px] h-[84px]"
+            className="w-[230px] h-[150px]"
             resizeMode="contain"
           />
           <Image
