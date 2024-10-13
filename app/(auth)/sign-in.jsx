8c2bc443d5c556e8290da358932217f286fdc693
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-10-13 13:41:37 +0200

    Add FormField.

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,9 +1,15 @@
 import { ScrollView, Text, View, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
+import FormField from "../../components/FormField";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -16,6 +22,19 @@ const SignIn = () => {
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Log in to Vidoo
           </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
