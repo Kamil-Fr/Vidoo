16da1821d97671fe11f87b1d62284a39f85aac21
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-11-18 14:55:36 +0100

    Cahneg sign-up file.

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,7 +1,7 @@
 import { ScrollView, Text, View, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
@@ -23,6 +23,7 @@ const SignUp = () => {
     setIsSubmitting(true);
     try {
       const result = await createUser(form.email, form.password, form.username);
+      router.replace("/home");
     } catch (error) {
       Alert.alert("Error", error.message);
     } finally {
