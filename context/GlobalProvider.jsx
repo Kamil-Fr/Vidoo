36d3994c25e14cc668152e919cf4d6efd0e988e8
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-11-23 23:19:30 +0100

    Add get CurrentUser function to appwrite file.

--- context/GlobalProvider.jsx
+++ context/GlobalProvider.jsx
@@ -7,5 +7,6 @@ const GlobalProvider = ({ children }) => {
   const [isLoggedIn] = useState(false);
   cosnt[(UserActivation, setUser)] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {}, []);
   return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
 };
