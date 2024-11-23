d603786afe1dea2afd437320f854145a73f1417e
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-11-23 23:27:38 +0100

    Modify GlobalProvider function in Global Provider file.

--- context/GlobalProvider.jsx
+++ context/GlobalProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import { getCurrentUser } from "../lib/appwrite";
 
 const GlobalContext = createContext();
 export const useGlobalContext = () => useContext(GlobalContext);
@@ -7,6 +8,23 @@ const GlobalProvider = ({ children }) => {
   const [isLoggedIn] = useState(false);
   cosnt[(UserActivation, setUser)] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getCurrentUser
+      .then((res) => {
+        if (res) {
+          setIsLoadingIn(true);
+          setUser(res);
+        } else {
+          setIsLoadingIn(false);
+          setUser(null);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoadingIn(false);
+      });
+  }, []);
   return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
 };
