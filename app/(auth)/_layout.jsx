b249f74e4daa57b57d1b6788f1cea631d6251154
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-11-24 23:43:56 +0100

    Resolving a problem.

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,19 +1,22 @@
 import { Text, View } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 import { ScreenStack } from "react-native-screens";
 import { StatusBar } from "expo-status-bar";
-import { GlobalProvider } from "../../context/GlobalProvider";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const AuthLayout = () => {
+  const { loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/home" />;
   return (
-    <GlobalProvider>
+    <>
       <Stack>
         <ScreenStack name="sign-in" options={{ headerShown: false }} />
         <ScreenStack name="sign-up" options={{ headerShown: false }} />
       </Stack>
       <StatusBar backgroundColor="#161622" style="light" />
-    </GlobalProvider>
+    </>
   );
 };
 
