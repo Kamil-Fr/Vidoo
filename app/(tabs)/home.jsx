6cf84ca781d2e433b82c4bdad875fba2b9f386ab
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2024-11-25 11:32:29 +0100

    Modify home file in tabs folder.

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={[{ id: 1 }]}
+        keyExctractor={(item) => item.$id}
+        renderItem={({ item }) => <Text>{item.id}</Text>}
+      />
+    </SafeAreaView>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({});
